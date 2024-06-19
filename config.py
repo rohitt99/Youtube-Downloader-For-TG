25c54d646e0db8e3c75589e52529afe1e5294162
rohitt99 <164199173+rohitt99@users.noreply.github.com>
2024-06-19 22:28:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import  os
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
-APP_ID = int(os.environ.get("API_ID"))
-API_HASH = os.environ.get("API_HASH")
+BOT_TOKEN = os.environ.get("6824943579:AAF5GUHPUA5nDZuspI1c24HZr3ywXOMPKI0")
+APP_ID = int(os.environ.get("25830228"))
+API_HASH = os.environ.get("a23a5133bddbdab87df3df06ccf63a89")
 G1N = os.environ.get("GROUP_1_NAME")
 G2N = os.environ.get("GROUP_2_NAME")
 G1L = os.environ.get("GROUP_1_LINK")
